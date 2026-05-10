4b23ed6ea68245d069f3c55f37de91965b8a68c4
Mauricio Barrera <kevinmauriciob3@gmail.com>
2026-05-09 20:21:02 -0500

    minimum fix

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from '../pages/Home'
-import Login from '../pages/Login'
+import Login from '../pages/auth/LoginPage'
 import ProtectedRoute from './ProtectedRoute'
 import { useAuth } from '../hooks/useAuth'
 
